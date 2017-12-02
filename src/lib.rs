9922d12bb44796f82ab66cc9fd9fa68378b81258
Shritesh Bhattarai <shr@ite.sh>
2017-12-02 09:22:34 -0600

    Add Parser

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,20 @@ enum Instruction {
     Open,
     Close,
 }
+
+fn parse(source: &str) -> Vec<Instruction> {
+    source
+        .chars()
+        .filter_map(|c| match c {
+            '>' => Some(Instruction::MoveRight),
+            '<' => Some(Instruction::MoveLeft),
+            '+' => Some(Instruction::Increment),
+            '-' => Some(Instruction::Decrement),
+            '.' => Some(Instruction::Output),
+            ',' => Some(Instruction::Input),
+            '[' => Some(Instruction::Open),
+            ']' => Some(Instruction::Close),
+            _ => None,
+        })
+        .collect()
+}

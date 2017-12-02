7a7c658f64ec1375f062f6b2607a0c27736933e1
Shritesh Bhattarai <shr@ite.sh>
2017-12-02 10:06:19 -0600

    Implement all but two instructions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::char;
+
 enum Instruction {
     MoveRight,
     MoveLeft,
@@ -24,4 +26,48 @@ fn parse(source: &str) -> Vec<Instruction> {
             _ => None,
         })
         .collect()
+}
+
+const MEMORY_SIZE: usize = 30000;
+
+pub fn run(source: &str, input: &str) -> String {
+    let instructions = parse(source);
+    let mut input_iter = input.chars();
+
+    let mut memory = [0u8; MEMORY_SIZE];
+
+    let mut instruction_counter: usize = 0;
+    let mut memory_counter: usize = 0;
+
+    let mut output = String::new();
+
+    while let Some(instruction) = instructions.get(instruction_counter) {
+        match *instruction {
+            Instruction::MoveRight => {
+                if memory_counter + 1 == MEMORY_SIZE {
+                    memory_counter = 0;
+                } else {
+                    memory_counter += 1;
+                }
+            }
+            Instruction::MoveLeft => {
+                if memory_counter == 0 {
+                    memory_counter = MEMORY_SIZE - 1;
+                } else {
+                    memory_counter -= 1;
+                }
+            }
+            Instruction::Increment => memory[memory_counter] += 1,
+            Instruction::Decrement => memory[memory_counter] -= 1,
+            Instruction::Output => {
+                output.push(char::from_u32(memory[memory_counter] as u32).unwrap())
+            }
+            Instruction::Input => memory[memory_counter] = input_iter.next().unwrap() as u8,
+            _ => unimplemented!(),
+        }
+
+        instruction_counter += 1;
+    }
+
+    output
 }
